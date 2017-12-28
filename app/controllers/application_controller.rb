10661872177ac28d7c0303aff6bd1ced73305ea6
Keegan Sheedy <ksheedy2@villanova.edu>
2017-12-28 04:51:50 +0000

    Add hello

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  
+  def hello
+    render html: "Hello, world... or most likely just me who will see this :("
+  end
+  
 end
